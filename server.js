670d92b4c797af00d2b9983be62a8016a0a72fcd
Romain Prieto <rprieto@Romains-MacBook-Air.local>
2013-07-21 01:32:19 +1000

    using stylus for CSS

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+var path = require('path');
 var express = require('express');
 var browserify = require('browserify-middleware');
+var stylus = require('stylus');
 
 var app = express();
 
-app.use('/js', browserify('./assets/js'));
-app.use(express.static(__dirname + '/public'));
+app.use('/js', browserify(path.join(__dirname, 'assets/js')));
+
+app.use(stylus.middleware({
+    compress: true,
+    src:  path.join(__dirname, 'assets'),
+    dest: path.join(__dirname, 'builtAssets')
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'builtAssets')));
 
 app.listen(process.env.PORT || 3000);
