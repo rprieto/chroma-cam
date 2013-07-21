76418a9cc3b3b45b4c64c1477de67e77fe644b86
Romain Prieto <rprieto@Romains-MacBook-Air.local>
2013-07-22 01:14:23 +1000

    colour calibration (using dominant colour on the webcam)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+var chroma = null;
 var blend  = null;
+var target = null;
 
 window.ChromaCam = {};
 
@@ -17,6 +19,31 @@ window.ChromaCam.loadBackground = function(e) {
     }
 };
 
+window.ChromaCam.calibrate = function(e) {
+    e.preventDefault();
+    
+    target.source = '#webcam';
+    
+    setTimeout(function() {
+    
+        var webcam = document.getElementById('output');
+        var canvas = document.createElement('canvas');
+        canvas.width  = webcam.width;
+        canvas.height = webcam.height;
+        var context = canvas.getContext('2d');
+        context.drawImage(webcam, 0, 0);
+    
+        var colorThief = new ColorThief();
+        var dominant = colorThief.getColor(canvas);
+        var rgb = 'rgb(' + dominant[0] + ',' + dominant[1] + ',' + dominant[2] + ')';
+        chroma.screen = rgb;
+        $('.icon-sign-blank.calibrated').css('color', rgb);
+        
+        target.source = blend;
+        
+    }, 500);
+}
+
 function openWebcam() {
     navigator.webkitGetUserMedia({video: true, audio: false}, function(localMediaStream) {
         var video = document.querySelector('#webcam');
@@ -29,11 +56,11 @@ function openWebcam() {
 function setupVideoProcessing() {
     var seriously = new Seriously();
     
-    var chroma = seriously.effect('chroma');
+    chroma = seriously.effect('chroma');
     chroma.source = '#webcam';
     chroma.weight = 1.32;
     chroma.balance = 0;
-    chroma.screen = 'rgb(77, 239, 41)';
+    chroma.screen = 'rgb(0, 0, 0)';
     chroma.clipWhite = 0.85;
     chroma.clipBlack = 0.5125;
 
@@ -41,8 +68,8 @@ function setupVideoProcessing() {
     blend.top = chroma;
     blend.bottom = document.querySelector('#backgroundVideo');
 
-    var target = seriously.target('#output');
-    target.source = blend;
+    target = seriously.target('#output');
+    target.source = '#webcam';
     
     seriously.go();
 }
